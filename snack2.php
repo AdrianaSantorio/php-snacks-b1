935ee0c2d264ea22f7f6baead9dc88b38ec1999b
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-28 09:18:20 +0100

    tried snack 2

--- snack2.php
+++ snack2.php
@@ -6,6 +6,49 @@ come valore un array di post associati a quella data. Stampare ogni data con i r
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
 */
 
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +58,26 @@ Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 2</title>
 </head>
 
 <body>
+    <h1>Snack 2</h1>
+
+    <ul>
+        <?php for ($i = 0; $i < count($posts); $i++) { ?>
+            <li>
+                <?= $posts[$i] ?>
+                <ul>
+                    <li>
+                        <h5><?= $posts[$i]['title'] ?></h5>
+                        <small><?= $posts[$i]['author'] ?></small>
+                        <p><?= $posts[$i]['text'] ?></p>
+                    </li>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
 
 </body>
 
